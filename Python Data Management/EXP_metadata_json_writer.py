7acef917e296b095ca15ab85546bd7068f9b0f41
Christopher Gettings <gettings.chris@gmail.com>
2022-01-31 14:04:16 -0500

    Adding VisOptions to Indicators.json Re: #9

--- Python Data Management/EXP_metadata_json_writer.py
+++ Python Data Management/EXP_metadata_json_writer.py
@@ -68,6 +68,115 @@ EXP_metadata_export = (
 )
 
 #-----------------------------------------------------------------------------------------#
+# nesting vis options
+#-----------------------------------------------------------------------------------------#
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+# map options
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+# On: 0/1
+# RankReverse: 0/1
+
+measure_mapping = (
+    EXP_metadata_export
+    .loc[:, 
+        [
+            "IndicatorID",
+            "MeasureID",
+            "Map",
+            "RankReverse"
+        ]
+    ]    
+    .drop_duplicates()
+    .rename(columns = {"Map": "On"})
+    .groupby(["IndicatorID", "MeasureID"], dropna = False)
+    .apply(lambda x: x[["On", "RankReverse"]].to_dict("records"))
+    .reset_index()
+    .rename(columns = {0: "Map"})
+)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+# trend options
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+# On: 0/1
+# Disparities: 0/1
+
+measure_trend = (
+    EXP_metadata_export
+    .loc[:, 
+        [
+            "IndicatorID",
+            "MeasureID",
+            "Trend",
+            "Disparities"
+        ]
+    ]    
+    .drop_duplicates()
+    .rename(columns = {"Trend": "On"})
+    .groupby(["IndicatorID", "MeasureID"], dropna = False)
+    .apply(lambda x: x[["On", "Disparities"]].to_dict("records"))
+    .reset_index()
+    .rename(columns = {0: "Trend"})
+)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+# linked measures
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+# ==== specific link view ==== #
+
+# because left-joining these 400 rows added 12k rows to the view
+
+MeasureID_links = (
+    pd.read_sql("SELECT * FROM MeasureID_links", EHDP_odbc)
+    .sort_values(by = ["BaseMeasureID", "MeasureID"])
+)
+
+# ==== nesting links ==== #
+
+measure_links = (
+    MeasureID_links
+    .drop_duplicates()
+    .groupby(["BaseMeasureID"], dropna = False)
+    .apply(lambda x: x[["MeasureID", "Axis"]].to_dict("records"))
+    .reset_index()
+    .rename(columns = {0: "Links", "BaseMeasureID": "MeasureID"})
+)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+# combining map, trend, and links, then nesting those under VisOptions
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+vis_options = (
+    pd.merge(
+        measure_mapping,
+        measure_trend,
+        how = "left"
+    )
+    .merge(
+        measure_links,
+        how = "left"
+    )
+    .groupby(
+        [
+            "IndicatorID",
+            "MeasureID"
+        ],
+        dropna = False
+    )
+    .apply(lambda x: x[[
+        "Map", 
+        "Trend", 
+        "Links"
+    ]].to_dict('records'))
+    .reset_index()
+    .rename(columns = {0: "VisOptions"})
+)
+
+
+#-----------------------------------------------------------------------------------------#
 # nesting geotypes
 #-----------------------------------------------------------------------------------------#
 
@@ -150,14 +259,19 @@ measure_times = (
     .rename(columns = {0: "AvailableTimes"})
 )
 
+
 #-----------------------------------------------------------------------------------------#
-# combining geotype and times, then nesting those under other measure-level info vars
+# combining geotype, times, and vis options, then nesting those under other measure-level info vars
 #-----------------------------------------------------------------------------------------#
 
 metadata = (
     pd.merge(
         measure_geotypes,
         measure_times,
+        how = "left"
+    )
+    .merge(
+        vis_options,
         how = "left"
     )
     .groupby(
@@ -176,11 +290,13 @@ metadata = (
         "Sources",
         "DisplayType",
         "AvailableGeographyTypes",
-        "AvailableTimes"
+        "AvailableTimes",
+        "VisOptions"
     ]].to_dict('records'))
     .reset_index()
     .rename(columns = {0: "Measures"})
 )
+
 
 #-----------------------------------------------------------------------------------------#
 # saving file
